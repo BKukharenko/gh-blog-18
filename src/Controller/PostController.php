3889240aef32a9e9e7a9a6f09c2fe9f614b258cc
BKukharenko <bohdankukharenko@gmail.com>
2018-12-20 20:40:48 +0200

    Created show-post route and template

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Form\PostType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -33,5 +34,17 @@ class PostController extends AbstractController
         return $this->render('post/create.html.twig', [
           'form' => $form->createView(),
         ]);
+    }
+
+  /**
+   * @Route("/post/{id}", name="show_post", requirements={"page"="\d+"})
+   * @ParamConverter("post", class="App\Entity\Post")
+   */
+  public function showPost(Post $post) {
+
+    return $this->render('post/show.html.twig', [
+      'post' => $post,
+      'category' => $post->getCategory(),
+    ]);
     }
 }
